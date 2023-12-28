f02013cb5ea5b11440fd22fbee92147f152bf6cf
dhruvshrm723@gmail.com <dhruvshrm723@gmail.com>
2023-12-28 23:03:19 +0530

    initial checking with dummey service

---
+++ client/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Dummey;
+using Grpc.Core;
+
+Console.WriteLine("Hello, World!");
+const string target = "127.0.0.1:50051";
+
+Channel channel = new(target, ChannelCredentials.Insecure);
+
+channel.ConnectAsync().ContinueWith((t) =>
+{
+    if (t.Status == TaskStatus.RanToCompletion) Console.WriteLine("The client connected successfullt");
+}).Wait();
+
+
+var client = new DummeyService.DummeyServiceClient(channel);
+
+await channel.ShutdownAsync();
+Console.ReadKey();
---
+++ server/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Core;
+
+Console.WriteLine("Hello, World!");
+const int Port = 50051;
+Server server = null;
+try
+{
+    server = new()
+    {
+        Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+
+    };
+
+    server.Start();
+    Console.WriteLine("The server is listning on post {0}", Port);
+    Console.ReadKey();
+
+}
+catch (IOException ex)
+{
+    Console.WriteLine("Server filed" + ex.Message);
+    throw;
+}
+finally
+{
+    if (server is not null) server.ShutdownAsync().Wait();
+}
